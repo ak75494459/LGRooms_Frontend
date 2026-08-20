4444e84ec88488ab7939b032a842b5d9ce59db15
Aryan kumar <aryhu@Aryans-MacBook-Air-107.local>
2026-08-21 03:22:44 +0530

    temporarily remove Auth0

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import AppRoutes from "./AppRoutes.tsx";
 import { BrowserRouter as Router } from "react-router-dom";
-import Auth0ProviderWithNavigate from "./auth/auth0ProviderWithNavigate.tsx";
+// import Auth0ProviderWithNavigate from "./auth/auth0ProviderWithNavigate.tsx";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Toaster } from "sonner";
 import ChatProvider from "./Context/ChatProvider.tsx";
@@ -18,12 +18,12 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById("root")!).render(
   <Router>
     <QueryClientProvider client={queryClient}>
-      <Auth0ProviderWithNavigate>
-        <ChatProvider>
-          <AppRoutes />
-          <Toaster visibleToasts={1} position="top-left" richColors />
-        </ChatProvider>
-      </Auth0ProviderWithNavigate>
+      {/* <Auth0ProviderWithNavigate> */}
+      <ChatProvider>
+        <AppRoutes />
+        <Toaster visibleToasts={1} position="top-left" richColors />
+      </ChatProvider>
+      {/* </Auth0ProviderWithNavigate> */}
     </QueryClientProvider>
-  </Router>
+  </Router>,
 );
